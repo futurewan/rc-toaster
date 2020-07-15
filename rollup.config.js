1cf889a395ac41b981c9ec6cfdef73f77946c9c3
futurewan <8937949+futurewan@users.noreply.github.com>
2020-07-15 17:57:17 +0800

    rollup配置优化

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,11 @@ import postcss from 'rollup-plugin-postcss';
 import resolve from 'rollup-plugin-node-resolve';
 
 import pkg from './package.json';
+const globalsObj = {
+  'react': 'React',
+  'react-dom': 'ReactDOM',
+  'prop-types': 'PropTypes'
+};
 
 export default [
   {
@@ -15,20 +20,22 @@ export default [
         format: 'cjs',
         sourcemap: false,
         exports: 'named',
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
+        globals: globalsObj
       },
       {
         file: pkg.module,
         format: 'es',
         sourcemap: false,
         exports: 'named',
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
+        globals: globalsObj
+      },
+      {
+        file: 'dist/toaster.umd.js',
+        format: 'umd',
+        name: 'Toaster',
+        sourcemap: false,
+        exports: 'named',
+        globals: globalsObj
       }
     ],
     plugins: [external(), postcss({}), babel({ exclude: 'node_modules/**' }), resolve(), commonjs()]
